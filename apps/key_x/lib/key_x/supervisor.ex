7948e73c592118f815b42c7fc3ce38d0b509caa9
coinshot <93342644+coinshot@users.noreply.github.com>
2022-09-17 19:05:37 +0200

    KeyX should not be multi tenant

--- apps/key_x/lib/key_x/supervisor.ex
+++ apps/key_x/lib/key_x/supervisor.ex
@@ -7,25 +7,9 @@ defmodule KeyX.TenantSupervisor do
 
   @impl true
   def init(args) do
-    registry = Keyword.fetch!(args, :registry)
-    tenants = Keyword.fetch!(args, :tenants)
-
-    register(registry)
-
     dynamic = []
     children = []
 
     Supervisor.init(children ++ dynamic, strategy: :one_for_one)
-  end
-
-  def callback(tenant), do: create_repo(tenant)
-
-  defp create_repo(tenant) do
-    KeyX.Repo.query('CREATE SCHEMA IF NOT EXISTS "#{tenant}"')
-    {:ok, _, _} = Ecto.Migrator.with_repo(KeyX.Repo, &Ecto.Migrator.run(&1, :up, all: true, prefix: tenant))
-  end
-
-  defp register(registry, weight \\ 10) do
-    Registry.register(registry, "start", {weight, __MODULE__, :callback})
   end
 end
--- apps/key_x/lib/release.ex
+++ apps/key_x/lib/release.ex
@@ -1,64 +1,18 @@
 defmodule KeyX.Release do
   @app :key_x
 
-  require Logger
-
   def migrate do
-    for tenant <- tenants() do
-      for repo <- repos() do
-        {:ok, _} = ensure_repo_created(repo, tenant)
-        {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true, prefix: tenant))
-      end
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
-    for tenant <- tenants() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version, prefix: tenant))
-    end
-  end
-
-  defp tenants() do
-    {:ok, _} = Application.ensure_all_started(:landlord)
-    case Landlord.Tenants.get() do
-      {:ok, tenants} -> tenants
-      {:error, :undefined_table} ->
-        Logger.error("could not retrieve tenants: Has Landlord been initialized?")
-        []
-      {:error, e} -> raise e
-    end
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
     Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
-  end
-
-  defp ensure_repo_created(repo, schema) do
-    {:ok, _} = Application.ensure_all_started(:ecto_sql)
-    {:ok, _} = Application.ensure_all_started(:postgrex)
-
-    opts = repo.config
-      |> Keyword.put(:backoff_type, :stop)
-      |> Keyword.put(:max_restarts, 0)
-
-    sql = 'CREATE SCHEMA IF NOT EXISTS "#{schema}";'
-
-    task = Task.Supervisor.async_nolink(Ecto.Adapters.SQL.StorageSupervisor, fn ->
-      {:ok, conn} = Postgrex.start_link(opts)
-
-      value = Postgrex.query(conn, sql, [], opts)
-      GenServer.stop(conn)
-      value
-    end)
-
-    case Task.yield(task, 5_000) || Task.shutdown(task) do
-      {:ok, {:ok, result}} ->
-        {:ok, result}
-      {:ok, {:error, error}} ->
-        {:error, error}
-      e ->
-        {:error, e}
-    end
   end
 end
