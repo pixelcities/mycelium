caf23f41346d81b6aecde90bfb02f2edf6e21c6f
coinshot <93342644+coinshot@users.noreply.github.com>
2023-04-02 12:48:23 +0200

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mycelium.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.1.13",
+      version: "0.1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
