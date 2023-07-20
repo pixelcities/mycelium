bbb5d5bc37d34460e69952f8249c1b49afb30a44
coinshot <93342644+coinshot@users.noreply.github.com>
2023-07-20 18:04:46 +0200

    Rollback to elixir 1.14

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mycelium.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.2.24",
+      version: "0.2.25",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
