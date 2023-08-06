8e029a7aac130efe8a642397521f0bd12ea0e023
coinshot <93342644+coinshot@users.noreply.github.com>
2023-08-06 17:26:21 +0200

    Update ssl_verify_fun

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mycelium.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.2.26",
+      version: "0.2.27",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
